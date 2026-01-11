67765c47f41eada929b5566a624ecc866f642990
JadeB01 <bar22102@byu.edu>
2026-01-10 18:01:02 -0700

    Made changes to prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage?: ");
+        string user = Console.ReadLine();
+        int percent = int.Parse(user);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+        
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed! Good job!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass, study hard next time!");
+        }
+
+
     }
 }
