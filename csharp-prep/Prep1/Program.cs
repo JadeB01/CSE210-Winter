1f2ab9b8984de5ce0a2278715472c76879c3131a
JadeB01 <bar22102@byu.edu>
2026-01-10 17:44:12 -0700

    made changes on prep 1.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string  first = Console.ReadLine();
+        //"Console.WriteLine()" is just like "Print" in Python//
+        //"Console.Write()" Like "Print" but no next sentence line(?) after//
+        //"String" is sjust like input for python. Create a keyword for the input your get, then "= Console.ReadLine();"//
+        Console.Write("Whats is your last name? ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {first} {last}. Nice to meet you!"); 
+        //if you want to use the keywards (Variables) that you created, use dollar sign "$"//
     }
 }
